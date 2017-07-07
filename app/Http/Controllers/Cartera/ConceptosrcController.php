c32b2525b4813dfb5fe2a58a34c09c538f158e1d
Daniel2506 <danieltrodriguez10@gmail.com>
2017-07-07 15:44:32 -0500

    conceptorc

--- app/Http/Controllers/Cartera/ConceptosrcController.php
+++ app/Http/Controllers/Cartera/ConceptosrcController.php
@@ -140,18 +140,19 @@ class ConceptosrcController extends Controller
                         DB::rollback();
                         return response()->json(['success' => false, 'errors' => 'No es posible recuperar plan de cuenta, verifique información ó por favor consulte al administrador.']);
                     }
-
-                    $documentos = Documentos::find($request->conceptosrc_documentos);
-                    if(!$documentos instanceof Documentos){
-                        DB::rollback();
-                        return response()->json(['success' => false, 'errors' => 'No es posible recuperar documento, verifique información ó por favor consulte al administrador.']);
+                    if ($request->has('conceptosrc_documentos')) {
+                        $documentos = Documentos::find($request->conceptosrc_documentos);
+                        if(!$documentos instanceof Documentos){
+                            DB::rollback();
+                            return response()->json(['success' => false, 'errors' => 'No es posible recuperar documento, verifique información ó por favor consulte al administrador.']);
+                        }
+                        $conceptosrc->conceptosrc_documentos = $documentos->id;
                     }
 
                     // conceptosrc
                     $conceptosrc->fill($data);
                     $conceptosrc->fillBoolean($data);
                     $conceptosrc->conceptosrc_plancuentas = $plancuentas->id;
-                    $conceptosrc->conceptosrc_documentos = $documentos->id;
                     $conceptosrc->save();;
 
                     //Forget cache
